94273b6911e75359d877eeaeeb1704151234de34
Otarion <c.garsault@laposte.net>
2024-02-02 10:41:28 +0100

    Atelier 4 : correction bug

--- src/Router.php
+++ src/Router.php
@@ -16,7 +16,7 @@ class Router {
         $this->request = $request;
 
         // Charge les routes depuis le fichier 'routes.php'.
-        $this->routes = include 'routes.php';
+        $this->routes = include '../routes.php';
     }
 
     // La méthode dispatch traite la requête en fonction des routes définies.
@@ -30,12 +30,12 @@ class Router {
         // Parcourt les routes définies pour trouver une correspondance.
         foreach ($this->routes as $route) {
             // Vérifie si la méthode et le chemin de la route correspondent à la requête.
-            if ($method === $route['method'] && preg_match('#^' . $route['path'] . '$#', $uri)) {
+            if ($method === $route->method && preg_match('#^' . $route->uri . '$#', $uri)) {
                 // Instancie le contrôleur spécifié dans la route.
-                $controller = new $route['controller'];
+                $controller = new $route ->controller;
                 
                 // Appelle la méthode d'action spécifiée dans la route.
-                $action = $route['action'];
+                $action = $route->action;
                 $controller->$action();
                 
                 // Termine la boucle car la route correspondante a été trouvée et traitée.
